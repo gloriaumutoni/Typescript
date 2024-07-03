be3864b7d36092f11f97246d410439e8c80a27ff
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-03 12:07:44 +0200

    objects and array

--- sandbox.js
+++ sandbox.js
@@ -3,3 +3,20 @@ var age = 32;
 var BlackBelt = false;
 var circ = function (diameter) { return diameter * Math.PI; };
 console.log(circ(3));
+var arr = ['first', 2, 'third'];
+arr.push('four');
+arr[0] = 1;
+console.log(arr);
+var obj = {
+    name: 'jay',
+    belt: 'red',
+    age: 34
+};
+obj.name = 'amanda';
+obj["fav"] = '1';
+obj = {
+    name: 'yoshin',
+    belt: 'black',
+    age: 23
+};
+console.log(obj);
--- sandbox.ts
+++ sandbox.ts
@@ -4,3 +4,23 @@ const BlackBelt=false
 
 const circ=(diameter:number)=>diameter*Math.PI
 console.log(circ(3))
+
+let arr=['first',2,'third']
+arr.push('four')
+arr[0]=1
+console.log(arr)
+
+let obj={
+  name:'jay',
+  belt:'red',
+  age:34
+}
+obj.name='amanda'
+obj["fav"]='1'
+
+ obj={
+  name:'yoshin',
+  belt:'black',
+  age:23
+ }
+ console.log(obj)
