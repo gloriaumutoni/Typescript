b449fb5705ad4d175b19ef69b59ee6f23b7939fb
gloriaumutoni <gloriaumutoni54@gmail.com>
2024-07-10 09:05:19 +0200

    using enums with interfaces and generics

--- classes/app.js
+++ classes/app.js
@@ -6,13 +6,24 @@ let gen = (obj) => {
 };
 let od = gen({ name: 'yoshi', age: 31 });
 console.log(od.age);
+// enums
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["Book"] = 0] = "Book";
+    ResourceType[ResourceType["Author"] = 1] = "Author";
+    ResourceType[ResourceType["Publisher"] = 2] = "Publisher";
+    ResourceType[ResourceType["Film"] = 3] = "Film";
+    ResourceType[ResourceType["Director"] = 4] = "Director";
+    ResourceType[ResourceType["Company"] = 5] = "Company";
+})(ResourceType || (ResourceType = {}));
 const docThree = {
     name: 'yoshi',
-    age: 32,
+    resoursetype: ResourceType.Book,
     data: ['middle aged man', 'with skills']
 };
 const docOne = {
     name: 'luigi',
-    age: 10,
+    resoursetype: ResourceType.Director,
     data: 'car racer'
 };
+console.log(docOne, docThree);
--- classes/app.ts
+++ classes/app.ts
@@ -7,22 +7,28 @@ let gen =<T extends {name:string}>(obj:T)=>{
 let od=gen({name:'yoshi',age:31})
 console.log(od.age)
 
+
+// enums
+enum ResourceType{ Book, Author, Publisher, Film, Director, Company}
+
+
 // Generics with interfaces
 
 interface Resource<T>{
     name:string,
-    age:number,
+    resoursetype:ResourceType,
     data:T
 }
  
 const docThree:Resource<string[]>={
     name:'yoshi',
-    age:32,
+    resoursetype:ResourceType.Book,
     data:['middle aged man','with skills']
 }
 
 const docOne:Resource<string>={
     name:'luigi',
-    age:10,
+    resoursetype:ResourceType.Director,
     data:'car racer'
 }
+console.log(docOne,docThree)
